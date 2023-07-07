c70f81b4f05b02ec6295b3dde1d73aa3c2b8dde7
liuyongshuai <liuyongshuai@hotmail.com>
2023-07-07 19:27:36 +0800

    add files

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+ * File:	main.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2023-07-07 19:26
+ ************************************************************************/
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdint.h>
+#include<iostream>
+
+int main(int argc, char *argv[]){
+    std::cout << "JUST TEST" << std::endl;
+    return 0;
+}
