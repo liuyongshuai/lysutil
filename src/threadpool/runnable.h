89314ccf5672d786ba8a17c9f2fcf35ac538857c
liuyongshuai <liuyongshuai@hotmail.com>
2023-07-09 19:22:38 +0800

    update~

--- src/threadpool/runnable.h
+++ src/threadpool/runnable.h
@@ -1,13 +1,13 @@
 /*************************************************************************
- * File:	common.h
+ * File:	runnable.h
  * Author:	liuyongshuai<liuyongshuai@hotmail.com>
  * Time:	2023-07-06 16:49
  ************************************************************************/
-#ifndef _PFSM_LOGSIM_COMMON_H_202307061649_
-#define _PFSM_LOGSIM_COMMON_H_202307061649_
+#ifndef _PFSM_LOGSIM_RUNNABLE_H_202307061649_
+#define _PFSM_LOGSIM_RUNNABLE_H_202307061649_
 
 namespace lysutil{
     namespace threadpool{
     }
 }
-#endif //_PFSM_LOGSIM_COMMON_H_202307061649_
+#endif //_PFSM_LOGSIM_RUNNABLE_H_202307061649_
