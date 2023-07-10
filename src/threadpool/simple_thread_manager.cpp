92fd79a2022bd2cc37a38e04762298cf613065f4
liuyongshuai <liuyongshuai@hotmail.com>
2023-07-10 19:28:42 +0800

    添加parkspace_recommend

---
+++ src/threadpool/simple_thread_manager.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+ * File:	simple_thread_manager.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2023-07-10 19:26
+ ************************************************************************/
+#include<iostream>
+#include "simple_thread_manager.h"
+
+namespace lysutil{
+    namespace threadpool{
+        
+    }
+} //namespace lysutil
---
+++ src/threadpool/simple_thread_manager.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+ * File:	simple_thread_manager.h
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2023-07-10 19:26
+ ************************************************************************/
+#ifndef _LYSUTIL_SIMPLE_THREAD_MANAGER_H_202307101926_
+#define _LYSUTIL_SIMPLE_THREAD_MANAGER_H_202307101926_
+
+#include<string>
+#include<vector>
+#include<map>
+
+namespace lysutil{
+    namespace threadpool{
+        class SimpleThreadManager{
+
+        };
+    }
+} //namespace lysutil
+
+
+#endif //_LYSUTIL_SIMPLE_THREAD_MANAGER_H_202307101926_
