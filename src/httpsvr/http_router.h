c779b58941d834c321837d49677348a994763a79
liuyongshuai <liuyongshuai@hotmail.com>
2023-07-12 18:04:39 +0800

    添加parkspace_recommend

--- src/httpsvr/http_router.h
+++ src/httpsvr/http_router.h
@@ -80,7 +80,7 @@ namespace lysutil{
 
             /**
              * 匹配正则路由，直接用正则表达式去匹配请求的URL，并把捕获的参数回传到Param配置里,如
-             * config 为 `^ggtest/aid(\w+?)/cid(\d+)$`，Param 为 aid=$1&cid=$2
+             * conf 为 `^ggtest/aid(\w+?)/cid(\d+)$`，Param 为 aid=$1&cid=$2
              * 则将请求中aid后面的字符串挑出来赋给aid，cid后面的字符串挑出来赋给cid
              */
             bool matchRegexpRouter(const std::string &uri, const routerItem *router, std::map< std::string, std::string > &args) const;
