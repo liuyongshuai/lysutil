f732598337490c695b647122e0ef59fb90260d08
liuyongshuai <liuyongshuai@hotmail.com>
2023-07-13 10:56:06 +0800

    添加parkspace_recommend

--- test/http_router_test.cpp
+++ test/http_router_test.cpp
@@ -12,27 +12,27 @@
 #include "httpsvr/http_request.h"
 #include "httpsvr/http_response.h"
 
-void httpFunc1(lysutil::httpsvr::httpRequest &req, lysutil::httpsvr::httpResponse &rsp) {
+void httpFunc1(lysutil::httpsvr::httpRequest &req, lysutil::httpsvr::httpResponse &rsp){
     std::cout << "httpFunc1" << std::endl;
 }
 
-void httpFunc2(lysutil::httpsvr::httpRequest &req, lysutil::httpsvr::httpResponse &rsp) {
+void httpFunc2(lysutil::httpsvr::httpRequest &req, lysutil::httpsvr::httpResponse &rsp){
     std::cout << "httpFunc2" << std::endl;
 }
 
-void printArgs(std::map<std::string, std::string> &args) {
+void printArgs(std::map< std::string, std::string > &args){
     std::cout << std::endl;
-    std::map<std::string, std::string>::const_iterator iter;
-    for (iter = args.begin(); iter != args.end(); iter++) {
+    std::map< std::string, std::string >::const_iterator iter;
+    for (iter = args.begin(); iter != args.end(); iter++){
         std::cout << iter->first << "\t" << iter->second << std::endl;
     }
     std::cout << std::endl;
 }
 
-int main(int argc, char *argv[]) {
-    std::map<std::string, std::string> args;
-    std::shared_ptr<lysutil::httpsvr::httpRouter> routers = lysutil::httpsvr::httpRouter::get_instance();
-    const lysutil::httpsvr::routerItem *rItem;
+int main(int argc, char *argv[]){
+    std::map< std::string, std::string > args;
+    std::shared_ptr< lysutil::httpsvr::httpRouter > routers = lysutil::httpsvr::httpRouter::get_instance();
+    std::shared_ptr< lysutil::httpsvr::routerItem > rItem{nullptr};
     routers->addRouter(lysutil::httpsvr::ROUTER_TYPE_PATH_INFO, "/abc/:aaaa:/:dddd", httpFunc1);
     routers->addRouter(lysutil::httpsvr::ROUTER_TYPE_PATH_INFO, "/abc/aaaa/dddd", httpFunc1);
     routers->addRouter(lysutil::httpsvr::ROUTER_TYPE_REGEXP, "/abcd/(\\d+)/(\\w+)", httpFunc2, "", "a=$1&b=$2");
