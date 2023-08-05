19f7c7eb2c045e06df829ae06ab21ecba39118af
liuyongshuai <liuyongshuai@hotmail.com>
2023-08-05 18:45:20 +0800

    modify files

--- test/md5_test.cpp
+++ test/md5_test.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]){
     std::string md51 = lysutil::comutils::strUtils::md5sum(str);
     std::cout << "md51=" << md51 << std::endl;
 
-    lysutil::comutils::md5Utils md5Utils;
+    lysutil::comutils::md5Utils md5Utils{};
     char buf[32] = {0};
     md5Utils.MD5Str(str.c_str(), str.size(), buf, 32);
     std::cout << "md52=" << buf << std::endl;
