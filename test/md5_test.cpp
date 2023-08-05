e3ecca248c0cd99ce818662ba58327ff2fc8b856
liuyongshuai <liuyongshuai@hotmail.com>
2023-08-05 14:49:58 +0800

    modify files

--- test/md5_test.cpp
+++ test/md5_test.cpp
@@ -10,6 +10,7 @@
 #include<stdint.h>
 #include "comutils/md5_utils.h"
 #include "comutils/str_utils.h"
+#include "comutils/http_utils.h"
 
 int main(int argc, char *argv[]){
     std::string str = "asdfasfda8743-t971-9834tlk=];....'.'";
@@ -20,5 +21,6 @@ int main(int argc, char *argv[]){
     char buf[32] = {0};
     md5Utils.MD5Str(str.c_str(), str.size(), buf, 32);
     std::cout << "md52=" << buf << std::endl;
+    std::cout << "genFixedRespDate=" << lysutil::comutils::httpUtils::genFixedRespDate(time(nullptr)) << std::endl;
     return 0;
 }
