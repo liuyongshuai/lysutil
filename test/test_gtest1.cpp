1e13b47c8fa435250a676d47a3263b4f003c40a3
liuyongshuai <liuyongshuai@hotmail.com>
2023-08-14 14:02:22 +0800

    modify files

--- test/test_gtest1.cpp
+++ test/test_gtest1.cpp
@@ -9,6 +9,7 @@
 #include<stdio.h>
 #include<stdint.h>
 #include <gtest/gtest.h>
+#include <leveldb/db.h>
 #include "snappy.h"
 
 int add(int a, int b) {
@@ -37,13 +38,42 @@ TEST(arithmetic, sub_test) {
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
 
-    std::string input{"123456789asdfghhjsssssssssssssssssssssssssssssssssswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"}, output;
+    std::string input{
+            "123456789asdfghhjsssssssssssssssssssssssssssssssssswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"}, output;
     snappy::Compress(input.data(), input.size(), &output);
     std::cout << "original size: " << input.size() << " Compressed size: " << output.size() << std::endl;
     std::string original;
     snappy::Uncompress(output.data(), output.size(), &original);
     std::cout << "original str: " << original << std::endl;
 
+    leveldb::DB *db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+    assert(status.ok());
+    //write key1,value1
+    std::string key = "key";
+    std::string value = "value";
+    status = db->Put(leveldb::WriteOptions(), key, value);
+    assert(status.ok());
+    status = db->Put(leveldb::WriteOptions(), key, "hello");
+    assert(status.ok());
+    status = db->Get(leveldb::ReadOptions(), key, &value);
+    assert(status.ok());
+    std::cout << value << std::endl;
+    std::string key2 = "key2";
+    //move the value under key to key2
+    status = db->Put(leveldb::WriteOptions(), key2, value);
+    assert(status.ok());
+    status = db->Delete(leveldb::WriteOptions(), key);
+    assert(status.ok());
+    status = db->Get(leveldb::ReadOptions(), key2, &value);
+    assert(status.ok());
+    std::cout << key2 << "===" << value << std::endl;
+    status = db->Get(leveldb::ReadOptions(), key, &value);
+    if (!status.ok()) std::cerr << key << "  " << status.ToString() << std::endl;
+    else std::cout << key << "===" << value << std::endl;
+    delete db;
 
     return RUN_ALL_TESTS();
 }
