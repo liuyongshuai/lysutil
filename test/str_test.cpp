86b84d5d29c6182a384586c8eb953493ca622ce1
liuyongshuai <liuyongshuai@hotmail.com>
2023-08-29 15:08:22 +0800

    修改行泊冲突的perceptionView定义

--- test/str_test.cpp
+++ test/str_test.cpp
@@ -34,8 +34,6 @@
 #include <libconfig.h++>
 #include <iomanip>
 #include<fontconfig/fontconfig.h>
-#include <gd.h>
-#include <gdfontl.h>
 #include "hiredis/hiredis.h"
 #include <mysql.h>
 #include <mysqld_error.h>
@@ -730,28 +728,6 @@ bool testFontconfig() {
         free(s);
     }
     return true;
-}
-
-int testGD() {
-    gdImagePtr im;
-    FILE *fp;
-    int cor_rad = 60;
-    im = gdImageCreateTrueColor(400, 400);
-    gdImageFilledRectangle(im, 0, 0, 399, 399, 0x00FFFFFF);
-
-    gdImageFilledArc(im, cor_rad, 399 - cor_rad, cor_rad * 2, cor_rad * 2, 90, 180, 0x0, gdPie);
-
-    fp = fopen("b.png", "wb");
-    if (!fp) {
-        fprintf(stderr, "Can't save png image.\n");
-        gdImageDestroy(im);
-        return 1;
-    }
-    gdImagePng(im, fp);
-    fclose(fp);
-
-    gdImageDestroy(im);
-    return 0;
 }
 
 int Send2Redis(const char *ip, int port, const char *key, const char *value, int len_value) {
