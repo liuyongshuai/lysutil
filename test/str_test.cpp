afb571f474f15f6e1492888d003c3c9a501b1d6e
liuyongshuai <liuyongshuai@hotmail.com>
2023-08-30 21:21:37 +0800

    修改行泊冲突的perceptionView定义

--- test/str_test.cpp
+++ test/str_test.cpp
@@ -61,8 +61,6 @@
 #include <ev.h>
 #include <nanomsg/pair.h>
 #include <nanomsg/nn.h>
-#include <absl/strings/string_view.h>
-#include "absl/strings/str_cat.h"
 #include "lua.hpp"
 
 
@@ -1905,33 +1903,6 @@ int testLibEV_Nanomsg() {
     return 0;
 }
 
-
-int testabseil() {
-    std::string s = "Tired like a dog";
-    absl::string_view sv(s);  // 用 std::string 初始化
-
-    const char *cs = "Hello world";
-    absl::string_view sv1(cs);  // 用字符串指针初始化
-    absl::string_view sv2(cs + 6, 5); // 用字符串区间初始化，输出 "world"
-    absl::string_view sv3(cs + 6, 8); // string_view 会自动判断字符串结尾，不会溢出，同样输出 "world"
-
-    absl::string_view sv4;
-    sv4 = sv3;   // string_view 之间可以直接赋值
-
-    std::cout << sv << std::endl;
-    std::cout << sv1 << std::endl;
-    std::cout << sv2 << std::endl;
-    std::cout << sv3 << std::endl;
-    std::cout << sv4 << std::endl;
-    std::cout << sv4[2] << std::endl;  // 可以用方括号获取string_view 中的字符
-    // sv4[2]  = 'i';  // ERROR：不允许通过方括号更改底层字符，因为string_view 不持有底层字符
-    std::string hello = "Hello";
-    std::string world = "world!";
-    std::string message = absl::StrCat(hello, ", ", world);
-    std::cout << message << std::endl;
-    return 0;
-}
-
 int testLua(int argc, char *argv[]) {
     ///< 创建lua句柄并初始化
     lua_State *pState = luaL_newstate();
@@ -2201,7 +2172,6 @@ int main(int argc, char *argv[]) {
     testunwind();
     testJsonCPP();
     testSQLite3();
-    testabseil();
     testLua(argc, argv);
 //    struct sockaddr_in addr;
 //    struct event_base *base = NULL;
