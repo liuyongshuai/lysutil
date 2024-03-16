1a762d275940af22c95006bf32511da7f00bf0d8
liuyongshuai <liuyongshuai@hotmail.com>
2024-03-16 18:58:56 +0800

    update

--- src/proxysvr/proxy_server.h
+++ src/proxysvr/proxy_server.h
@@ -14,7 +14,7 @@ namespace lysutil {
     /**
      * 代理服务，转发请求
      * 1、工作机制是用户端 -->  proxysvr -->  client  --> 具体的服务。
-     * 2、每个client都要有一个唯一的名称name、服务类型type(ssh/type)。
+     * 2、每个client都要有一个唯一的名称name、服务类型type(ssh/http)。
      * 3、proxysvr监听时每个端口号只能对应一个client，要不然都不知道要请求哪。
      * 4、在底层实现时以name、服务类型区分。
      * 5、代理服务要是可动态配置的，服务可提供接口增删监听的端口号。
