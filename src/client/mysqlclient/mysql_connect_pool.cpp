9fac8260922d39014160d9d5b612ee8c43b2da0e
liuyongshuai <liuyongshuai@hotmail.com>
2024-03-16 13:04:05 +0800

    update

---
+++ src/client/mysqlclient/mysql_connect_pool.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * File:	mysql_connect_pool.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2024-03-16 13:03
+ ************************************************************************/
+#include<iostream>
+#include "client/mysqlclient/mysql_connect_pool.h"
+
+namespace lysutil {
+    namespace client {
+    }
+} //namespace lysutil
---
+++ src/client/mysqlclient/mysql_connect_pool.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+ * File:	mysql_connect_pool.h
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2024-03-16 13:03
+ ************************************************************************/
+#ifndef _LYSUTIL_MYSQL_CONNECT_POOL_H_202403161303_
+#define _LYSUTIL_MYSQL_CONNECT_POOL_H_202403161303_
+
+#include<string>
+#include<vector>
+#include<map>
+
+namespace lysutil {
+    namespace client {
+        class MySQLConnectPool {
+
+        };
+    }
+} //namespace lysutil
+
+
+#endif //_LYSUTIL_MYSQL_CONNECT_POOL_H_202403161303_
