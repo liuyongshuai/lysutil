493f194325f38db31e7f90b1972f17cd40a306e8
liuyongshuai <liuyongshuai@hotmail.com>
2025-04-02 10:45:43 +0800

    update

--- dump_scrapy.go
+++ dump_scrapy.go
@@ -93,11 +93,11 @@ func main() {
 		}
 		for _, row := range rows {
 			var articleInfo Article
-			articleInfo.AutoID = row["auto_id"].ToUint64()
-			articleInfo.Tags = row["tags"].ToString()
-			articleInfo.Content = row["Content"].ToString()
-			articleInfo.Title = row["title"].ToString()
-			articleInfo.URL = row["url"].ToString()
+			articleInfo.AutoID, _ = row["auto_id"].ToUint64()
+			articleInfo.Tags, _ = row["tags"].ToString()
+			articleInfo.Content, _ = row["Content"].ToString()
+			articleInfo.Title, _ = row["title"].ToString()
+			articleInfo.URL, _ = row["url"].ToString()
 			articleAutoId = articleInfo.AutoID
 			b, e := json.Marshal(articleInfo)
 			if e != nil {
