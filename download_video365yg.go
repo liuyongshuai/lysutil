19e1178c16943d16a0e969d9f530ffd556703b43
liuyongshuai <liuyongshuai@hotmail.com>
2025-04-11 12:27:51 +0800

    add files

--- download_video365yg.go
+++ download_video365yg.go
@@ -158,6 +158,7 @@ func downloadVideo(auto_id uint64, video_id string) {
 	}
 	if vSize == 0 {
 		fmt.Println("invalid vSize")
+		fmt.Println(vlist)
 		return
 	}
 	db.Execute(videoUpdateSQL, vSize, auto_id)
