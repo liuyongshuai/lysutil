ca2f7ba14629acd9c3641ca87d25721f3feb19c4
liuyongshuai <liuyongshuai@hotmail.com>
2025-04-11 16:37:47 +0800

    add files

--- download_video365yg.go
+++ download_video365yg.go
@@ -112,80 +112,82 @@ func main() {
 }
 
 func downloadVideo() {
-	vid, ok := <-videoChan
-	if !ok {
-		return
-	}
-	auto_id := vid.AutoID
-	video_id := vid.VideoID
-	t := time.Now().UnixNano() / 1000000
-	callback := "reqwest_" + strconv.FormatUint(uint64(t), 10)
-	url := video_url + video_id
-	um, _ := negoutils.ParseUrl(url, -1)
-	path := um["path"]
-	s := crc32.ChecksumIEEE([]byte(path + "?r=" + strconv.FormatUint(uint64(t), 10)))
-	url += "?r=" + strconv.FormatUint(uint64(t), 10) + "&s=" + strconv.FormatUint(uint64(s), 10) + "&callback=" + callback
-	//fmt.Println(url)
-	//fmt.Println("\n\n")
-
-	client := negoutils.NewHttpClient(url, context.Background())
-	client.SetReferer("http://365yg.com")
-	client.AddHeader("myHeaderKey", "myHeaderValue")
-	client.SetUserAgent("Mozilla/5.0 (Linux; Android 6.0.1; SM919 Build/MXB48T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.84 Mobile Safari/537.36 JsSdk/2 NewsArticle/6.2.7 NetType/wifi")
-	client.SetKeepAlive(false)
-	resp, e := client.Get()
-	if e != nil {
-		fmt.Println("get url failed", e)
-		return
-	}
-	ret := resp.GetBodyString()
-	ret = strings.TrimLeft(ret, callback)
-	ret = strings.TrimLeft(ret, "(")
-	ret = strings.TrimRight(ret, ")")
-	//fmt.Println(ret)
-	//fmt.Println("\n\n")
-	vinfo := TouTiaoVideo365YGInfo{}
-	e = json.Unmarshal([]byte(ret), &vinfo)
-	if e != nil {
-		fmt.Println("json.Unmarshal vinfo failed", e)
-		return
-	}
-
-	//是否被平台封禁
-	if vinfo.Code != 0 {
-		_, _, e = db.Execute(videoForbidSQL, auto_id)
-		fmt.Println("FORBIDDEN", e, " video_id=", video_id, "message=", vinfo.Message, ", auto_id=", auto_id)
-		return
-	}
-
-	//fmt.Println(vinfo)
-	//fmt.Println("\n\n")
-	//tmpDecode, _ := base64.StdEncoding.DecodeString(vinfo.Data.PostUrl)
-	//vinfo.Data.PostUrl = negoutils.ByteToStr(tmpDecode)
-	vlist := vinfo.Data.VideoList
-	//mainUrl := ""
-	vSize := int64(0)
-	for _, v := range vlist {
-		//tmpDecode, e := base64.StdEncoding.DecodeString(v.MainURL)
-		//if e != nil {
-		//	continue
-		//}
-		//mainUrl = negoutils.ByteToStr(tmpDecode)
-		if v.Size > 0 {
-			vSize = v.Size
-			break
+	for {
+		vid, ok := <-videoChan
+		if !ok {
+			return
 		}
-		//fmt.Println("mainUrl", mainUrl)
-		//fmt.Println("videoSize", v.Size)
-	}
-	if vSize == 0 {
-		fmt.Println("invalid vSize")
+		auto_id := vid.AutoID
+		video_id := vid.VideoID
+		t := time.Now().UnixNano() / 1000000
+		callback := "reqwest_" + strconv.FormatUint(uint64(t), 10)
+		url := video_url + video_id
+		um, _ := negoutils.ParseUrl(url, -1)
+		path := um["path"]
+		s := crc32.ChecksumIEEE([]byte(path + "?r=" + strconv.FormatUint(uint64(t), 10)))
+		url += "?r=" + strconv.FormatUint(uint64(t), 10) + "&s=" + strconv.FormatUint(uint64(s), 10) + "&callback=" + callback
+		//fmt.Println(url)
+		//fmt.Println("\n\n")
+
+		client := negoutils.NewHttpClient(url, context.Background())
+		client.SetReferer("http://365yg.com")
+		client.AddHeader("myHeaderKey", "myHeaderValue")
+		client.SetUserAgent("Mozilla/5.0 (Linux; Android 6.0.1; SM919 Build/MXB48T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.84 Mobile Safari/537.36 JsSdk/2 NewsArticle/6.2.7 NetType/wifi")
+		client.SetKeepAlive(false)
+		resp, e := client.Get()
+		if e != nil {
+			fmt.Println("get url failed", e)
+			continue
+		}
+		ret := resp.GetBodyString()
+		ret = strings.TrimLeft(ret, callback)
+		ret = strings.TrimLeft(ret, "(")
+		ret = strings.TrimRight(ret, ")")
+		//fmt.Println(ret)
+		//fmt.Println("\n\n")
+		vinfo := TouTiaoVideo365YGInfo{}
+		e = json.Unmarshal([]byte(ret), &vinfo)
+		if e != nil {
+			fmt.Println("json.Unmarshal vinfo failed", e)
+			continue
+		}
+
+		//是否被平台封禁
+		if vinfo.Code != 0 {
+			_, _, e = db.Execute(videoForbidSQL, auto_id)
+			fmt.Println("FORBIDDEN", e, " video_id=", video_id, "message=", vinfo.Message, ", auto_id=", auto_id)
+			continue
+		}
+
 		//fmt.Println(vinfo)
-		return
-	}
-	_, _, e = db.Execute(videoUpdateSQL, vSize, auto_id)
-	if e != nil {
-		fmt.Println("update VideoSize failed", e)
-		return
+		//fmt.Println("\n\n")
+		//tmpDecode, _ := base64.StdEncoding.DecodeString(vinfo.Data.PostUrl)
+		//vinfo.Data.PostUrl = negoutils.ByteToStr(tmpDecode)
+		vlist := vinfo.Data.VideoList
+		//mainUrl := ""
+		vSize := int64(0)
+		for _, v := range vlist {
+			//tmpDecode, e := base64.StdEncoding.DecodeString(v.MainURL)
+			//if e != nil {
+			//	continue
+			//}
+			//mainUrl = negoutils.ByteToStr(tmpDecode)
+			if v.Size > 0 {
+				vSize = v.Size
+				break
+			}
+			//fmt.Println("mainUrl", mainUrl)
+			//fmt.Println("videoSize", v.Size)
+		}
+		if vSize == 0 {
+			fmt.Println("invalid vSize")
+			//fmt.Println(vinfo)
+			continue
+		}
+		_, _, e = db.Execute(videoUpdateSQL, vSize, auto_id)
+		if e != nil {
+			fmt.Println("update VideoSize failed", e)
+			continue
+		}
 	}
 }
